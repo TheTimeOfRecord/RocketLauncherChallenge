58bf5c4c632778fa317abb4b1b3efa482443866e
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-10-15 16:13:31 +0900

    Add script

--- Assets/Scripts/Challenge/RocketControllerC.cs
+++ Assets/Scripts/Challenge/RocketControllerC.cs
@@ -1,10 +1,15 @@
+using System;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
 public class RocketControllerC : MonoBehaviour
 {
     private EnergySystemC _energySystem;
     private RocketMovementC _rocketMovement;
+
+    public Action<float> OnBoostEvent;
+    public Action<Axis> OnMoveEvent;
     
     private bool _isMoving;
     private float _movementDirection;
@@ -29,8 +34,16 @@ public class RocketControllerC : MonoBehaviour
 
     // OnMove 구현
     // private void OnMove...
+    private void OnMove(InputValue value)
+    {
+        OnMoveEvent?.Invoke(value);
+    }
 
 
     // OnBoost 구현
     // private void OnBoost...
+    private void OnBoost()
+    {
+
+    }
 }
